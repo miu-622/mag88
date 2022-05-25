ac29e35e508923dda5908302609125e57a72b4f1
miu-622 <93674703+miu-622@users.noreply.github.com>
2022-05-26 01:54:04 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+// topに戻るボタン //
+$(function(){
+    $('footer h2').click(function(){
+        $('html, body').animate({ 
+          'scrollTop': 0 
+        }, 500);
+      });
+// topに戻るボタン //
+    }
+)
